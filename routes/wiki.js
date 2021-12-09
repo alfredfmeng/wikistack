aa31db9c4d353c14a88f1501da3cebb24aaa5453
Alfred Meng <alfredfmeng@gmail.com>
2021-12-09 13:49:09 -0600

    Refacted POST route to redirect to newly created post. Refactored GET slug route to res.send wikiPage with provided slug

--- routes/wiki.js
+++ routes/wiki.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const addPage = require("../views/addPage");
 const { Page } = require("../models");
+const { wikiPage } = require("../views");
 
 router.get("/", (req, res, next) => {
   res.send("This is the GET request for root for wiki.js");
@@ -10,19 +11,28 @@ router.get("/add", (req, res, next) => {
   res.send(addPage());
 });
 
-router.get("/:slug", (req, res, next) => {
-  res.send(`hit dynamic route at ${req.params.slug}`);
+router.get("/:slug", async (req, res, next) => {
+  const slug = req.params.slug;
+  try {
+    const page = await Page.findOne({
+      where: {
+        slug: slug,
+      },
+    });
+    res.send(wikiPage(page));
+  } catch (error) {
+    next(error);
+  }
 });
 
 router.post("/", async (req, res, next) => {
   const { name, title, email, content, status } = req.body;
-
   try {
     const page = await Page.create({
       title: title,
       content: content,
     });
-    res.redirect("/");
+    res.redirect(`/wiki/${page.slug}`);
   } catch (error) {
     next(error);
   }
