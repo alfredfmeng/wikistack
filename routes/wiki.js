e19b8cb316df67c8b80a33f8eb6a5d74d9a93a5a
Alfred Meng <alfredfmeng@gmail.com>
2021-12-09 11:54:26 -0600

    Created routes for users and wiki

---
+++ routes/wiki.js
@@ -0,0 +1,33 @@
+const router = require("express").Router();
+const addPage = require("../views/addPage");
+const { Page } = require("../models");
+
+router.get("/", (req, res, next) => {
+  res.send("This is the GET request for root for wiki.js");
+});
+
+router.get("/add", (req, res, next) => {
+  res.send(addPage());
+});
+
+router.post("/", async (req, res, next) => {
+  const { name, title, email, content, status } = req.body;
+
+  const generateSlug = () => {
+    console.log("THIS IS REGEX", title.replace(/\s+/g, "_").replace(/\W/g, ""));
+  };
+
+  generateSlug();
+
+  //   try {
+  //     const page = await Page.create({
+  //       title: title,
+  //       content: content,
+  //     });
+  //     res.redirect("/");
+  //   } catch (error) {
+  //     next(error);
+  //   }
+});
+
+module.exports = router;
