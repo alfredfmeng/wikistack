ec9cd4f05ec3a98ea73305c6bbee3095f2fc20b6
Alfred Meng <alfredfmeng@gmail.com>
2021-12-10 15:11:17 -0600

    Updated POST route to create a User instance in the database and also associated author to a page instance

--- routes/wiki.js
+++ routes/wiki.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const addPage = require("../views/addPage");
-const { Page } = require("../models");
+const { Page, User } = require("../models");
 const { wikiPage, main } = require("../views");
 
 router.get("/", async (req, res, next) => {
@@ -33,10 +33,20 @@ router.get("/:slug", async (req, res, next) => {
 router.post("/", async (req, res, next) => {
   const { name, title, email, content, status } = req.body;
   try {
+    const [user, created] = await User.findOrCreate({
+      where: {
+        name: name,
+        email: email,
+      },
+    });
+
     const page = await Page.create({
       title: title,
       content: content,
     });
+
+    await page.setAuthor(user);
+
     res.redirect(`/wiki/${page.slug}`);
   } catch (error) {
     next(error);
