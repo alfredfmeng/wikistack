52eeb33708bee400684235159ec17ef71ec09d0f
Alfred Meng <alfredfmeng@gmail.com>
2021-12-10 15:19:32 -0600

    Created GET route to fetch all users from database. Began scaffolding /:id GET route

--- routes/users.js
+++ routes/users.js
@@ -1,3 +1,21 @@
 const router = require("express").Router();
+const { User } = require("../models");
+const { userList } = require("../views");
+
+router.get("/", async (req, res, next) => {
+  try {
+    const users = await User.findAll();
+    res.send(userList(users));
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/:id", async (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error);
+  }
+});
 
 module.exports = router;
