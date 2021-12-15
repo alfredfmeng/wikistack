cb42d05105b7c785be856bc4dd71b562d8ea7c59
Alfred Meng <alfredfmeng@gmail.com>
2021-12-14 21:14:25 -0600

    Began coding out GET route for /:slug/edit

--- routes/wiki.js
+++ routes/wiki.js
@@ -36,6 +36,19 @@ router.get("/:slug", async (req, res, next) => {
   }
 });
 
+router.get("/:slug/edit", async (req, res, next) => {
+  const slug = req.params.slug;
+  try {
+    const page = await Page.findOne({
+      where: {
+        slug: slug,
+      },
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
 router.post("/", async (req, res, next) => {
   const { name, title, email, content, status } = req.body;
   try {
