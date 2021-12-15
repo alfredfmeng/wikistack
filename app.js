e7992a090e8a63dcbb9d836f616bf450cf36a60f
Alfred Meng <alfredfmeng@gmail.com>
2021-12-15 17:19:20 -0600

    Added methodOverride middleware to express app

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
-const layout = require("./views/layout");
-const { db, Page, User } = require("./models");
+const { db } = require("./models");
 const wikiRouter = require("./routes/wiki");
 const userRouter = require("./routes/users");
+const methodOverride = require("method-override");
 
 db.authenticate().then(() => {
   console.log("connected to the database");
@@ -13,6 +13,7 @@ db.authenticate().then(() => {
 app.use(morgan("dev"));
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride("_method"));
 
 app.use("/wiki", wikiRouter);
 app.use("/users", userRouter);
